2f9612533e8fef860cb274b224b94fa8e0d7493b
pojanicha <pojanicha.sora@bumail.net>
2026-04-15 17:05:13 +0700

    First Push (Have Movement, Camera))

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpForce = 6.5f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool doubleJump;
+    [SerializeField] private float doubleJumpForce = 6.5f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (IsGrounded() && !Input.GetButton("Jump"))
+        {
+            doubleJump = false;
+        }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+           if (IsGrounded() || doubleJump)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, doubleJump ? doubleJumpForce : jumpForce);
+
+                doubleJump = !doubleJump;
+            }
+        }
+
+        /*if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+        */
+
+        Flip();
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        { 
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+
+
+        }
+    
+    }
+
+
+  
+}

10f8abba360ff14e3706eefa5d0042ea1fb36578
pojanicha <pojanicha.sora@bumail.net>
2026-04-20 00:00:17 +0700

    Moving Platform (Success)

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform[] points;
+    public float moveSpeed = 5f;
+    private int index;
+    private int direction = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[index].position) < 0.1f)
+        {
+            if (index == points.Length - 1)
+            {
+                direction = -1;
+            }
+            else if (index == 0)
+            {
+                direction = 1;
+            }
+
+            index += direction;
+        }
+
+       
+
+        transform.position = Vector2.MoveTowards(transform.position, points[index].position, moveSpeed * Time.deltaTime);
+    }
+}

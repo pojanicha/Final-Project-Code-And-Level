9b440c178fb841eec17ba060d78d7d1c1af79960
pojanicha <pojanicha.sora@bumail.net>
2026-04-20 01:25:33 +0700

    Make Prefabs

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static bool isPaused {get; private set;} = false;
+
+    public static void SetPause(bool pause)
+    {
+        isPaused = pause;
+    }
+
+}
